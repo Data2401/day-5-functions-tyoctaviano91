d4e7ed54588be7945cfa8d3362cd4ba29d271bb9
Tyler Guerrero <tyleroctaviano@gmail.com>
2026-02-01 22:55:28 -0600

    Began Part II

--- InClassDay5_Guerrero.R
+++ InClassDay5_Guerrero.R
@@ -14,12 +14,16 @@
 
 # Assign your name to a variable called `my_name`
 my_name <- Tyler
+
 # Assign the number of pets you have to a variable called `num_pets`
 num_pets <- 1
+
 # Assign the title of your favorite book as a variable called `favorite_book`
 favorite_book <- "Stormlight Archive"
+
 # Create a variable called `radius`, which is your favorite number
 radius <- 13
+
 # Create a variable called `area`, which is the area of a circle with that radius.
 # Use the variable `radius` to calculate this. 
 # Use the built-in constant `pi` for pi.
@@ -33,14 +37,18 @@ too_big <- area > 100
 # Display the variable `my_name`
 # Display the variable `area` 
 # Display the variable `too_big`
-
+print(my_name)
+print(area)
+print(too_big)
 
 ### PART TWO ###
 
 # Create a variable `name_length` that holds how many letters (including spaces)
 # are in your name, using the variable you made above. (use the `nchar()` function)
+name_length <- nchar(5)
 
 # Print the number of letters in your name
+print(name_length)
 
 # Create a variable `now_doing` that is your name followed by "is programming!" 
 # (use the `paste()` function)
